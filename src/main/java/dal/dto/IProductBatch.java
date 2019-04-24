100a3fd60227a71246e0321e6f72d69fb74d0bc5
Siff <siff.ravn@gmail.com>
2019-04-24 23:10:00 +0200

    ProductBatch interface partly implemented

--- src/main/java/dal/dto/IProductBatch.java
+++ src/main/java/dal/dto/IProductBatch.java
@@ -1,4 +1,28 @@
 package dal.dto;
 
 public interface IProductBatch {
+
+    //ikke færdig, døjer med nogle overvejelser omkring relevante attributter... - siff
+
+    int productBatchID;
+    int productID;
+    int recipeID;
+    int statusID;
+
+    int getProductBatchID();
+
+    void setProductBatchID(int productBatchID);
+
+    int getProductID();
+
+    void setProductID(int productID);
+
+    int getRecipeID();
+
+    void setRecipeID(int recipeID);
+
+    int getStatusID();
+
+    void setStatusID(int statusID);
+
 }
