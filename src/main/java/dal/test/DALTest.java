19e0c8a78957b62eac6a847f168f46625f5cb25b
magnusbrok <43874955+magnusbrok@users.noreply.github.com>
2019-04-24 17:40:50 +0200

    Few changes plus added test package and class

---
+++ src/main/java/dal/test/DALTest.java
@@ -0,0 +1,31 @@
+package dal.test;
+
+import dal.dao.IUserDAO;
+import dal.dto.IUser;
+import dal.dto.User;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DALTest {
+
+
+    @Test
+    public void UserTest() {
+
+
+    }
+
+    @Test
+    void RecipeTest() {
+
+    }
+
+
+
+}
