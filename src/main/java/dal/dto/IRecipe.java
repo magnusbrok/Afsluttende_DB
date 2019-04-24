a121be3f44626e1f1bb4206ede6d1794e6997855
Siff <siff.ravn@gmail.com>
2019-04-24 14:53:04 +0200

    RecipeDTO interface implemented

--- src/main/java/dal/dto/IRecipe.java
+++ src/main/java/dal/dto/IRecipe.java
@@ -1,4 +1,30 @@
 package dal.dto;
 
+import java.util.List;
+
 public interface IRecipe {
+
+    int getRecepiID();
+
+    void setRecepiID(int recepiID);
+
+    int getProductID();
+
+    void setProductID(int productID);
+
+    int getQuantity();
+
+    void setQuantity(int quantity);
+
+    int getDeviation();
+    void setDeviation(int deviation);
+
+    List<IIngredientDTO> getIngredientList();
+
+    void setIngredientList(List<IIngredientDTO> ingredientList);
+
+    void addIngredient(IIngredientDTO ingredient);
+
+    void removeIngredient(IIngredientDTO ingredient);
+
 }
