ff8e0d1d3acf14eeacdceac0fafa993fd828e0ae
TMJ97 <s185027@student.dtu.dk>
2019-05-02 13:50:09 +0200

    Update RecipeDAOTest.java

--- src/main/java/dal/test/RecipeDAOTest.java
+++ src/main/java/dal/test/RecipeDAOTest.java
@@ -1,12 +1,41 @@
 package dal.test;
 
+import dal.dao.RecipeDAO;
+import dal.dao.interfaces.IRecipeDAO;
+import dal.dao.interfaces.IUserDAO;
+import dal.dto.Recipe;
+import dal.dto.User;
+import dal.dto.interfaces.IRecipe;
+import dal.dto.interfaces.IUser;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class RecipeDAOTest {
+    IRecipeDAO recipeDAO = new RecipeDAO();
 
     @Test
-    public void test() {
+    public void RecipeTest() {
+        Recipe testRecipe = new Recipe();
+        testRecipe.setRecipeID(11);
+        testRecipe.setTitle("UnitTEST");
+        testRecipe.setProductID(1);
+        testRecipe.setQuantity(500);
 
-
+        try {
+            recipeDAO.createRecipe(testRecipe,1);
+            IRecipe receivedRecipe = recipeDAO.getRecipe(testRecipe.getRecipeID());
+            assertEquals(testRecipe.getTitle(), receivedRecipe.getTitle());
+            assertEquals(testRecipe.getProductID(), receivedRecipe.getProductID());
+            assertEquals(testRecipe.getQuantity(), receivedRecipe.getQuantity());
+            recipeDAO.deleteRecipe(11);
+        }catch (IUserDAO.DALException e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
